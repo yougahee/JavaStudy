e8bffa3242eb71a9aa32937bbda9595aaa5c2aac
ggahui <dbrkgml97@nate.com>
2020-07-31 23:32:32 +0900

    multi Thread

---
+++ README.md
@@ -0,0 +1,8 @@
+**JAVA STUDY**
+
+##### fast campus lecture 
+about java
+
+- chapter 15
+   1. About Thread
+   2. Multi Thread
--- chapter15/LibraryMain.java
+++ chapter15/LibraryMain.java
@@ -8,26 +8,33 @@ class Library{
     public Library() {
         books.add("태백산맥 1");
         books.add("태백산맥 2");
-        books.add("태백산맥 3");
+        books.add("태백산맥 3");/*
         books.add("태백산맥 4");
         books.add("태백산맥 5");
         books.add("태백산맥 6");
-        books.add("태백산맥 7");
+        books.add("태백산맥 7");*/
     }
 
-    public String lendBook() {
+    public synchronized String lendBook() throws InterruptedException {
         Thread t = Thread.currentThread();
+
+        while ( books.size() == 0 ) {
+            System.out.println(t.getName() + "waiting start");
+            wait();
+            System.out.println(t.getName() + "waiting end");
+        }
 
         String title = books.remove(0);
         System.out.println(t.getName() + ":" + title + " lend");
         return title;
     }
 
-    public void returnBook(String title) {
+    public synchronized void returnBook(String title) {
         Thread t = Thread.currentThread();
-        books.add(title);
 
-        System.out.println(t.getName() + ":" + title + " lend");
+        books.add(title);
+        notifyAll();
+        System.out.println(t.getName() + ":" + title + " return");
     }
 }
 
@@ -35,10 +42,11 @@ class Student extends Thread{
     public void run() {
         try {
             String title = LibraryMain.library.lendBook();
+
+            if(title == null) return;
             sleep(5000);
             LibraryMain.library.returnBook(title);
-
-        }catch (InterruptedException e) {
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
@@ -52,9 +60,16 @@ public class LibraryMain {
         Student std1 = new Student();
         Student std2 = new Student();
         Student std3 = new Student();
+        Student std4 = new Student();
+        Student std5 = new Student();
+        Student std6 = new Student();
 
         std1.start();
         std2.start();
         std3.start();
+        std4.start();
+        std5.start();
+        std6.start();
+
     }
 }
